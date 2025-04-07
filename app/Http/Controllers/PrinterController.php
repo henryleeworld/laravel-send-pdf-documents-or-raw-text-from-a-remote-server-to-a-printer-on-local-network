e6df419de9a2e598bc7fece96012ea5862334d92
Henry Lee <henryleeworld@gmail.com>
2025-04-08 00:22:22 +0800

    feat:升級至 laravel 11

--- app/Http/Controllers/PrinterController.php
+++ app/Http/Controllers/PrinterController.php
@@ -13,19 +13,19 @@ class PrinterController extends Controller
      */
     public function index()
     {
-        $printerId = '72112713';
+        $printerId = '73166646';
         // List all printers
         $printers = Printing::printers();
         foreach ($printers as $printer) {
-            echo '印表機編號：' . $printer->id() . PHP_EOL;
-            echo '印表機名稱：' . $printer->name() . PHP_EOL;
-            echo '印表機狀態：' . $printer->status() . PHP_EOL;
+            echo __('Printer ID:') . $printer->id() . PHP_EOL;
+            echo __('Printer Name:') . $printer->name() . PHP_EOL;
+            echo __('Printer Status:') . $printer->status() . PHP_EOL;
             echo PHP_EOL;
         }
         $printJob = Printing::newPrintTask()
                     ->printer($printerId)
-                    ->file(storage_path('data/covid-19-vaccine-research-findings.pdf'))
+                    ->file(storage_path('data/sudan-red-measures.pdf'))
                     ->send();
-        echo '印表機伺服器工作回傳編號數字：' . $printJob->id() . PHP_EOL;
+        echo __('Print job id:') . $printJob->id() . PHP_EOL;
     }
 }
